4c2f6ce3e6461919687c10c0ec63fabe1428aebc
Wenzel P. P. Peppmeyer <wenzel.peppmeyer@gmx.de>
2017-08-20 10:28:29 +0200

    show main PID on exit

--- run-this.sh
+++ run-this.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 
-perl6 bin/moar-stress-main.p6
+trap '{ echo "moar-stress-main.p6 pid was: $MOAR_STRESS_MAIN_PID"; exit 255; }' INT
+
+perl6 bin/moar-stress-main.p6 &
+MOAR_STRESS_MAIN_PID=$!
+
+wait $MOAR_STRESS_MAIN_PID
+
